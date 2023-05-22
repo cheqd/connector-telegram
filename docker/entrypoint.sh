04ceb3d183df05fad491d3c7379c10a14e82fb43
Filip Djokic <djokicf@protonmail.com>
2023-05-22 16:18:17 +0200

    Remove node user

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,27 +3,24 @@
 set -euo pipefail
 
 # Create blank custom CA certificate file
-sudo touch /usr/local/share/ca-certificates/do-cert.crt
+touch /usr/local/share/ca-certificates/do-cert.crt
 
 # Insert custom CA certificate contents from environment variable into file
-echo "$DO_CA_CERT" | sudo tee /usr/local/share/ca-certificates/do-cert.crt > /dev/null
+echo "$DO_CA_CERT" | tee /usr/local/share/ca-certificates/do-cert.crt > /dev/null
 
 # Update CA certificates
-sudo update-ca-certificates
-
-# Remove node user from sudoers group
-sudo sed -i '$ d' /etc/sudoers
+update-ca-certificates
 
 if [[ $# -eq 0 ]]; then
   # No flags passed, executing npm start
   npm start
 else
-  while getopts "a:" opt; do
+  while getopts "db:" opt; do
     case "$opt" in
-      a)
+      db)
         # Flag a for passed for executing DB alteration commands
         version_number=${OPTARG}
-        echo "Executing actions for --alter-db for version $version_number"
+        echo "Executing db alterations for version $version_number"
         npx @logto/cli db seed all --swe
         npx @logto/cli db alteration deploy "$version_number"
         ;;
